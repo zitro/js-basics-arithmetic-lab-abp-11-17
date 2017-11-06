4565ed96b17ce69c15a7effc9fcf119e5d09a310
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:24:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let randomNumber = Math.floor(Math.random() * 10) + 1;
 let randomInteger = Math.floor(randomNumber);
 
 let  min = Math.ceil(1000000001);
-let  max = Math.floor(1000000020);
+//let  max = Math.floor(1000000020);
+
 let randomUserID = Math.floor(Math.random() * (max - min)) + min;
   //The maximum is exclusive and the minimum is inclusive
