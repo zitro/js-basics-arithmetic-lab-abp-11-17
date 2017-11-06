8b6096949d9d9d064d0b68c36a06610c6641f862
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-06 02:07:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ let newID = oldID + (10 **9 );
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = Math.floor(Math.random() * 20) + 0;
 let randomInteger = Math.floor(randomNumber);
+let randomUserID = number.isInteger(randomInteger);
 
 let  min = Math.ceil(1000000001);
 let  max = Math.floor(1000000020);
